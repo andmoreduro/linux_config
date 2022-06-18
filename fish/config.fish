828c02ffa436a0e094180e5097a1d6262ce15927
andmoreduro <71203480+andmoreduro@users.noreply.github.com>
2022-06-18 16:33:49 -0500

    Better fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,22 @@
-set fish_greeting
-set -a PATH /home/andresm/bin
-set EDITOR /usr/bin/micro
+# First line removes the path; second line sets it.  Without the first line,
+# your path gets massive and fish becomes very slow.
+set -e fish_user_paths
+set -U fish_user_paths $HOME/.local/bin $HOME/bin $HOME/.cargo/bin $HOME/flutter/bin $HOME/Android/Sdk/cmdline-tools/latest $HOME/Android/Sdk/platform-tools $fish_user_paths
+set -x CHROME_EXECUTABLE /usr/bin/google-chrome-stable
 
+### EXPORT ###
+set fish_greeting                                 # Supresses fish's intro message
+set TERM "xterm-256color"                         # Sets the terminal type
+set EDITOR "vim"                                  # $EDITOR use vim
+
+### SET EITHER DEFAULT EMACS MODE OR VI MODE ###
+function fish_user_key_bindings
+  # fish_default_key_bindings
+  fish_vi_key_bindings
+end
+### END OF VI MODE ###
+
+### FUNCTIONS ###
 # Functions needed for !! and !$
 function __history_previous_command
   switch (commandline -t)
@@ -21,8 +36,9 @@ function __history_previous_command_arguments
     commandline -i '$'
   end
 end
+
 # The bindings for !! and !$
-if $fish_key_bindings = fish_vi_key_bindings
+if [ "$fish_key_bindings" = "fish_vi_key_bindings" ];
   bind -Minsert ! __history_previous_command
   bind -Minsert '$' __history_previous_command_arguments
 else
@@ -83,24 +99,30 @@ end
 ### END OF FUNCTIONS ###
 
 ### ALIASES ###
-alias ..='cd ..'             # Go back 1 time
-alias .2='cd ../..'          # ''  ''  2  ''
-alias .3='cd ../../..'       # ''  ''  3  ''
-alias .4='cd ../../../..'    # ''  ''  4  ''
-alias .5='cd ../../../../..' # ''  ''  5  ''
+# Navigation
+alias ..='cd ..'
+alias .2='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
 
 # Changing "ls" to "exa"
 alias list='exa -al --color=always --group-directories-first' # my preferred listing
-alias la='exa -a --color=always --group-directories-first'    # all files and dirs
-alias ll='exa -l --color=always --group-directories-first'    # long format
-alias lt='exa -aT --color=always --group-directories-first'   # tree listing
+alias la='exa -a --color=always --group-directories-first'  # all files and dirs
+alias ll='exa -l --color=always --group-directories-first'  # long format
+alias lt='exa -aT --color=always --group-directories-first' # tree listing
+alias l.='exa -a | egrep "^\."'
 
-# pacman and yay
-alias pacsyu='sudo pacman -Syyu'                 # update only standard pkgs
-alias yaysua='yay -Sua --noconfirm'              # update only AUR pkgs
-alias yaysyu='yay -Syu --noconfirm'              # update standard pkgs and AUR pkgs
+
+# For pacman and yay
+alias pacsyu='sudo pacman -Syu'                  # update only standard pkgs
+alias pacsyyu='sudo pacman -Syyu'                # Refresh pkglist & update standard pkgs
+alias yaysua='yay -Sua --noconfirm'              # update only AUR pkgs (yay)
+alias yaysyu='yay -Syu --noconfirm'              # update standard pkgs and AUR pkgs (yay)
+# alias parsua='paru -Sua --noconfirm'             # update only AUR pkgs (paru)
+# alias parsyu='paru -Syu --noconfirm'             # update standard pkgs and AUR pkgs (paru)
 alias unlock='sudo rm /var/lib/pacman/db.lck'    # remove pacman lock
-alias cleanup='sudo pacman -Rns (pacman -Qtdq)'  # remove orphaned packages
+alias cleanup='sudo pacman -Rns (pacman -Qtdq)' # remove orphaned packages
 
 # Get fastest mirrors
 alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
@@ -113,25 +135,13 @@ alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
 
-# Get top process eating memory
-alias psmem='ps auxf | sort -nr -k 4'
-alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+# Confirm before overwriting something
+alias cp="cp -i"
+alias mv='mv -i'
+alias rm='rm -i'
+### END OF ALIASES ###
 
-# Get top process eating cpu ##
-alias pscpu='ps auxf | sort -nr -k 3'
-alias pscpu10='ps auxf | sort -nr -k 3 | head -10'
+### SETTING THE STARSHIP PROMPT ###
+starship init fish | source
 
-# youtube-dl
-alias yta-aac="youtube-dl --extract-audio --audio-format aac "
-alias yta-best="youtube-dl --extract-audio --audio-format best "
-alias yta-flac="youtube-dl --extract-audio --audio-format flac "
-alias yta-m4a="youtube-dl --extract-audio --audio-format m4a "
-alias yta-mp3="youtube-dl --extract-audio --audio-format mp3 "
-alias yta-opus="youtube-dl --extract-audio --audio-format opus "
-alias yta-vorbis="youtube-dl --extract-audio --audio-format vorbis "
-alias yta-wav="youtube-dl --extract-audio --audio-format wav "
-alias ytv-best="youtube-dl -f bestvideo+bestaudio "
-
-### STARSHIP SHELL ###
-starship init fish | source 
 
